698b465bc9ce0bba0214c82fb883812d4e18eab7
Martin Skagh <martin.skagh@gmail.com>
2025-03-16 20:32:40 +0100

    started to implement an autotiling. The tileset is added and the grafical coordinates for 47 unique tiles resides in the drawTile function of a scene. Left to implement is a bitmask to determin witch tile to draw

--- game/scene.js
+++ game/scene.js
@@ -23,68 +23,159 @@ export default class Scene {
 
     #drawTile(x, y, tile) {
         const context = this.context
-        const tileSize = this.tileSize
+        const ts = this.tileSize
+        let col = this.tileSize
+        let row = this.tileSize
         switch (tile) {
             case 0:
-                context.drawImage(this.tileImage, 0, 0, 32, 32, x * tileSize, y * tileSize, tileSize, tileSize);
+                context.drawImage(this.tileImage, col * 0, row * 0, ts, ts, x * ts, y * ts, ts, ts);
                 break
             case 1:
-                context.drawImage(this.tileImage, 32, 0, 32, 32, x * tileSize, y * tileSize, tileSize, tileSize);
+                context.drawImage(this.tileImage, col * 1, row * 0, ts, ts, x * ts, y * ts, ts, ts);
                 break
             case 2:
-                context.drawImage(this.tileImage, 64, 0, 32, 32, x * tileSize, y * tileSize, tileSize, tileSize);
+                context.drawImage(this.tileImage, col * 2, row * 0, ts, ts, x * ts, y * ts, ts, ts);
                 break
             case 3:
-                context.drawImage(this.tileImage, 96, 0, 32, 32, x * tileSize, y * tileSize, tileSize, tileSize);
+                context.drawImage(this.tileImage, col * 3, row * 0, ts, ts, x * ts, y * ts, ts, ts);
                 break
             case 4:
-                context.drawImage(this.tileImage, 0, 32, 32, 32, x * tileSize, y * tileSize, tileSize, tileSize);
+                context.drawImage(this.tileImage, col * 4, row * 0, ts, ts, x * ts, y * ts, ts, ts);
                 break
             case 5:
-                context.drawImage(this.tileImage, 32, 32, 32, 32, x * tileSize, y * tileSize, tileSize, tileSize);
-
+                context.drawImage(this.tileImage, col * 5, row * 0, ts, ts, x * ts, y * ts, ts, ts);
                 break
             case 6:
-                context.drawImage(this.tileImage, 64, 32, 32, 32, x * tileSize, y * tileSize, tileSize, tileSize);
+                context.drawImage(this.tileImage, col * 6, row * 0, ts, ts, x * ts, y * ts, ts, ts);
                 break
             case 7:
-                context.drawImage(this.tileImage, 96, 32, 32, 32, x * tileSize, y * tileSize, tileSize, tileSize);
-
+                context.drawImage(this.tileImage, col * 7, row * 0, ts, ts, x * ts, y * ts, ts, ts);
                 break
             case 8:
-                context.drawImage(this.tileImage, 0, 64, 32, 32, x * tileSize, y * tileSize, tileSize, tileSize);
-
+                context.drawImage(this.tileImage, col * 8, row * 0, ts, ts, x * ts, y * ts, ts, ts);
+                break
+            case 9:
+                context.drawImage(this.tileImage, col * 9, row * 0, ts, ts, x * ts, y * ts, ts, ts);
                 break
             case 10:
-                context.drawImage(this.tileImage, 32, 64, 32, 32, x * tileSize, y * tileSize, tileSize, tileSize);
-
+                context.drawImage(this.tileImage, col * 0, row * 1, ts, ts, x * ts, y * ts, ts, ts);
                 break
             case 11:
-                context.drawImage(this.tileImage, 64, 64, 32, 32, x * tileSize, y * tileSize, tileSize, tileSize);
-
+                context.drawImage(this.tileImage, col * 1, row * 1, ts, ts, x * ts, y * ts, ts, ts);
                 break
             case 12:
-                context.drawImage(this.tileImage, 96, 64, 32, 32, x * tileSize, y * tileSize, tileSize, tileSize);
-
+                context.drawImage(this.tileImage, col * 2, row * 1, ts, ts, x * ts, y * ts, ts, ts);
                 break
             case 13:
-                context.drawImage(this.tileImage, 0, 96, 32, 32, x * tileSize, y * tileSize, tileSize, tileSize);
-
+                context.drawImage(this.tileImage, col * 3, row * 1, ts, ts, x * ts, y * ts, ts, ts);
                 break
             case 14:
-                context.drawImage(this.tileImage, 32, 96, 32, 32, x * tileSize, y * tileSize, tileSize, tileSize);
-
+                context.drawImage(this.tileImage, col * 4, row * 1, ts, ts, x * ts, y * ts, ts, ts);
                 break
             case 15:
-                context.drawImage(this.tileImage, 64, 96, 32, 32, x * tileSize, y * tileSize, tileSize, tileSize);
-
+                context.drawImage(this.tileImage, col * 5, row * 1, ts, ts, x * ts, y * ts, ts, ts);
                 break
             case 16:
-                context.drawImage(this.tileImage, 96, 96, 32, 32, x * tileSize, y * tileSize, tileSize, tileSize);
-
+                context.drawImage(this.tileImage, col * 6, row * 1, ts, ts, x * ts, y * ts, ts, ts);
                 break
-            default:
-
+            case 17:
+                context.drawImage(this.tileImage, col * 7, row * 1, ts, ts, x * ts, y * ts, ts, ts);
+                break
+            case 18:
+                context.drawImage(this.tileImage, col * 8, row * 1, ts, ts, x * ts, y * ts, ts, ts);
+                break
+            case 19:
+                context.drawImage(this.tileImage, col * 9, row * 1, ts, ts, x * ts, y * ts, ts, ts);
+                break
+            case 20:
+                context.drawImage(this.tileImage, col * 0, row * 2, ts, ts, x * ts, y * ts, ts, ts);
+                break
+            case 21:
+                context.drawImage(this.tileImage, col * 1, row * 2, ts, ts, x * ts, y * ts, ts, ts);
+                break
+            case 22:
+                context.drawImage(this.tileImage, col * 2, row * 2, ts, ts, x * ts, y * ts, ts, ts);
+                break
+            case 23:
+                context.drawImage(this.tileImage, col * 3, row * 2, ts, ts, x * ts, y * ts, ts, ts);
+                break
+            case 24:
+                context.drawImage(this.tileImage, col * 4, row * 2, ts, ts, x * ts, y * ts, ts, ts);
+                break
+            case 25:
+                context.drawImage(this.tileImage, col * 5, row * 2, ts, ts, x * ts, y * ts, ts, ts);
+                break
+            case 26:
+                context.drawImage(this.tileImage, col * 6, row * 2, ts, ts, x * ts, y * ts, ts, ts);
+                break
+            case 27:
+                context.drawImage(this.tileImage, col * 7, row * 2, ts, ts, x * ts, y * ts, ts, ts);
+                break
+            case 28:
+                context.drawImage(this.tileImage, col * 8, row * 2, ts, ts, x * ts, y * ts, ts, ts);
+                break
+            case 29:
+                context.drawImage(this.tileImage, col * 9, row * 2, ts, ts, x * ts, y * ts, ts, ts);
+                break
+            case 30:
+                context.drawImage(this.tileImage, col * 0, row * 3, ts, ts, x * ts, y * ts, ts, ts);
+                break
+            case 31:
+                context.drawImage(this.tileImage, col * 1, row * 3, ts, ts, x * ts, y * ts, ts, ts);
+                break
+            case 32:
+                context.drawImage(this.tileImage, col * 2, row * 3, ts, ts, x * ts, y * ts, ts, ts);
+                break
+            case 33:
+                context.drawImage(this.tileImage, col * 3, row * 3, ts, ts, x * ts, y * ts, ts, ts);
+                break
+            case 34:
+                context.drawImage(this.tileImage, col * 4, row * 3, ts, ts, x * ts, y * ts, ts, ts);
+                break
+            case 35:
+                context.drawImage(this.tileImage, col * 5, row * 3, ts, ts, x * ts, y * ts, ts, ts);
+                break
+            case 36:
+                context.drawImage(this.tileImage, col * 6, row * 3, ts, ts, x * ts, y * ts, ts, ts);
+                break
+            case 37:
+                context.drawImage(this.tileImage, col * 7, row * 3, ts, ts, x * ts, y * ts, ts, ts);
+                break
+            case 38:
+                context.drawImage(this.tileImage, col * 8, row * 3, ts, ts, x * ts, y * ts, ts, ts);
+                break
+            case 39:
+                context.drawImage(this.tileImage, col * 9, row * 3, ts, ts, x * ts, y * ts, ts, ts);
+                break
+            case 40:
+                context.drawImage(this.tileImage, col * 0, row * 4, ts, ts, x * ts, y * ts, ts, ts);
+                break
+            case 41:
+                context.drawImage(this.tileImage, col * 1, row * 4, ts, ts, x * ts, y * ts, ts, ts);
+                break
+            case 42:
+                context.drawImage(this.tileImage, col * 2, row * 4, ts, ts, x * ts, y * ts, ts, ts);
+                break
+            case 43:
+                context.drawImage(this.tileImage, col * 3, row * 4, ts, ts, x * ts, y * ts, ts, ts);
+                break
+            case 44:
+                context.drawImage(this.tileImage, col * 4, row * 4, ts, ts, x * ts, y * ts, ts, ts);
+                break
+            case 45:
+                context.drawImage(this.tileImage, col * 5, row * 4, ts, ts, x * ts, y * ts, ts, ts);
+                break
+            case 46:
+                context.drawImage(this.tileImage, col * 6, row * 4, ts, ts, x * ts, y * ts, ts, ts);
+                break
+            case 47:
+                context.drawImage(this.tileImage, col * 7, row * 4, ts, ts, x * ts, y * ts, ts, ts);
+                break
+            case 48:
+                context.drawImage(this.tileImage, col * 8, row * 4, ts, ts, x * ts, y * ts, ts, ts);
+                break
+            case 49:
+                context.drawImage(this.tileImage, col * 9, row * 4, ts, ts, x * ts, y * ts, ts, ts);
                 break
         }
     }
